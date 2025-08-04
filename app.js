f9ca35d5d351cf3c4ac80674b51c5774c25c023d
KWAshen <ashen.maliaha@gmail.com>
2025-08-04 23:11:30 +0200

    foto postman

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static('express'));
 
 
 app.get('/',(req,res) => {
@@ -15,31 +16,31 @@ app.get('/bacheca',(req,res) => {
     {
       titolo:'Titolo 1',
       contenuto:"lorem",
-      immagine:'img/img.png',
+      immagine:'immagini/img.png',
       tag:['calcio','tennis','basket','pallavolo','nuoto'],
     },
     {
       titolo:'Titolo 2',
       contenuto:"lorem",
-      immagine:'img/img.png',
+      immagine:'immagini/img.png',
       tag:['calcio','tennis','basket','pallavolo','nuoto'],
     },
     {
       titolo:'Titolo 3',
       contenuto:"lorem",
-      immagine:'img/img.png',
+      immagine:'immagini/img.png',
       tag:['calcio','tennis','basket','pallavolo','nuoto'],
     },
     {
       titolo:'Titolo 4',
       contenuto:"lorem",
-      immagine:'img/img.png',
+      immagine:'immagini/img.png',
       tag:['calcio','tennis','basket','pallavolo','nuoto'],
     },
     {
       titolo:'Titolo 5',
       contenuto:"lorem",
-      immagine:'img/img.png',
+      immagine:'immagini/img.png',
       tag:['calcio','tennis','basket','pallavolo','nuoto'],
     }
   ];
