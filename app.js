378e77e8c03473fe1de13d9890215373fd76e58f
KWAshen <ashen.maliaha@gmail.com>
2025-08-04 22:14:25 +0200

    lista post

--- app.js
+++ app.js
@@ -10,6 +10,41 @@ app.get('/',(req,res) => {
   res.send('Server del mio blog');
 })
 
+app.get('/2',(req,res) => {
+  const posts= [
+    {
+      titolo:'Titolo 1',
+      contenuto:"lorem",
+      immagine:'.img/post.jpg',
+      tag:['calcio','tennis','basket','pallavolo','nuoto'],
+    },
+    {
+      titolo:'Titolo 2',
+      contenuto:"lorem",
+      immagine:'.img/post.jpg',
+      tag:['calcio','tennis','basket','pallavolo','nuoto'],
+    },
+    {
+      titolo:'Titolo 3',
+      contenuto:"lorem",
+      immagine:'.img/post.jpg',
+      tag:['calcio','tennis','basket','pallavolo','nuoto'],
+    },
+    {
+      titolo:'Titolo 4',
+      contenuto:"lorem",
+      immagine:'.img/post.jpg',
+      tag:['calcio','tennis','basket','pallavolo','nuoto'],
+    },
+    {
+      titolo:'Titolo 5',
+      contenuto:"lorem",
+      immagine:'.img/post.jpg',
+      tag:['calcio','tennis','basket','pallavolo','nuoto'],
+    },
+  ]
+})
+
 app.listen(port,()=>{
   console.log(`Server is running on http://localhost:${port}`);
 });
